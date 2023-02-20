40505d0ef612000a2d70ba9c15bf974f4bf4a8b3
Hunter-Pittman <hunterpittman@protonmail.com>
2023-02-20 11:20:01 -0800

    Added storage lcoation setup (still need to pass to router)

--- config/envConfig.go
+++ config/envConfig.go
@@ -1,6 +1,38 @@
-package envConfig
+package config
 
-func Setup() {
-	// Setup the environment
-	// Create file storage directory root
+import (
+	"os"
+	"runtime"
+)
+
+func Setup() string {
+	if runtime.GOOS == "windows" {
+		dir := "C:\\tonic\\storage\\"
+
+		_, err := os.Stat(dir)
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(dir, 0755)
+			if err != nil {
+				panic(err)
+			}
+		} else if err != nil {
+			panic(err)
+		}
+		return dir
+	} else {
+		// This is the linux path creation to determine storage locations
+		dir := "/src/tonic/storage/"
+
+		_, err := os.Stat(dir)
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(dir, 0755)
+			if err != nil {
+				panic(err)
+			}
+		} else if err != nil {
+			panic(err)
+		}
+
+		return dir
+	}
 }
