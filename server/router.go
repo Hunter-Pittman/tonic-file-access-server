fbc1112433644d23ae959f32a1a348379b4f1200
Hunter-Pittman <hunterpittman@protonmail.com>
2023-02-20 10:46:42 -0800

    File upload now works (use curl for testing postman errors)

--- server/router.go
+++ server/router.go
@@ -1,24 +1,23 @@
 package server
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"tonic-file-access-server/middlewares/auth"
+	"path/filepath"
 	"tonic-file-access-server/middlewares/logger"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func NewRouter() *gin.Engine {
 	// Env variables
 
-	dst := "C:\\Users\\hunte\\Documents\\repos\\tonic-file-access-server\\uploads"
+	//dst := "C:\\test"
 
 	router := gin.Default()
 
 	router.Use(logger.RequestID())
-	router.Use(auth.TokenAuthMiddleware())
+	//router.Use(auth.TokenAuthMiddleware())
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -26,37 +25,33 @@ func NewRouter() *gin.Engine {
 		})
 	})
 
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.POST("/upload", func(c *gin.Context) {
-		// Multipart form
-		form, err := c.MultipartForm()
+		file, err := c.FormFile("file")
 
+		// The file cannot be received.
 		if err != nil {
-			log.Fatal(err)
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "No file is received",
+			})
+			return
 		}
 
-		files := form.File["upload[]"]
+		// Retrieve file information
+		extension := filepath.Ext(file.Filename)
+		// Generate random file name for the new uploaded file so it doesn't override the old file with same name
+		newFileName := uuid.New().String() + extension
 
-		for _, file := range files {
-			log.Println(file.Filename)
-
-			// Upload the file to specific dst.
-			c.SaveUploadedFile(file, dst)
+		// The file is received, so let's save it
+		if err := c.SaveUploadedFile(file, "C:\\test\\"+newFileName); err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"message": "Unable to save the file",
+			})
+			return
 		}
-		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
-	})
 
-	router.POST("/uploadsingle", func(c *gin.Context) {
-		// Multipart form
-		form, _ := c.MultipartForm()
-		files := form.File["upload[]"]
-
-		for _, file := range files {
-			log.Println(file.Filename)
-
-			// Upload the file to specific dst.
-			c.SaveUploadedFile(file, dst)
-		}
-		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
+		// File saved successfully. Return proper result
+		c.JSON(http.StatusOK, gin.H{"message": "Your file has been successfully uploaded."})
 	})
 
 	return router
