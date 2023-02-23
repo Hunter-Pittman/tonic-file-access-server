001c30fcc6ea2da4ea8f02b3389377545213dd76
Hunter-Pittman <hunterpittman@protonmail.com>
2023-02-22 17:56:41 -0800

    Added multi upload support for the server.

--- server/router.go
+++ server/router.go
@@ -78,7 +78,6 @@ func NewRouter(apiToken string) *gin.Engine {
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.POST("/upload", func(c *gin.Context) {
 		file, err := c.FormFile("file")
-
 		if err != nil {
 			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 				"message": "No file is received",
@@ -93,7 +92,24 @@ func NewRouter(apiToken string) *gin.Engine {
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "Your file has been successfully uploaded at /download-user-file/" + file.Filename})
+		c.JSON(http.StatusOK, gin.H{"message": "Your file has been successfully uploaded at /download/" + file.Filename})
+	})
+
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.POST("/multiupload", func(c *gin.Context) {
+		// Multipart form
+		form, _ := c.MultipartForm()
+		files := form.File["file[]"]
+
+		for _, file := range files {
+			if err := c.SaveUploadedFile(file, dst+file.Filename); err != nil {
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+					"message": "Unable to save the file",
+				})
+				return
+			}
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Your file has been successfully uploaded at /download/[FILENAME]"})
 	})
 
 	router.GET("/download-user-file/:filename", func(c *gin.Context) {
